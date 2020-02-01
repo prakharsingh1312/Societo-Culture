69a7c030dc687e2ae38898e9b6e80289512a98bb
Prakhar Singh <prakharsingh13@gmail.com>
2020-02-01 12:36:56 +0530

    Added DBconfig file

---
+++ dbconfig.php
@@ -0,0 +1,9 @@
+<?php
+	$dbHost = "http://35.225.148.206/";
+	$dbDatabase = "hackowasp";
+	$dbPasswrod = "Popat#Panda@1234$";
+	$dbUser = "Hackowasp";
+
+
+	$mysqli = mysqli_connect($dbHost, $dbUser, $dbPasswrod, $dbDatabase);
+?>
