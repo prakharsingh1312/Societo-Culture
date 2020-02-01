ab71c23cd73a0214f2c13f1b463a56a019bd8cae
ccstiet <ccs@thapar.edu>
2020-02-01 18:35:31 +0530

    front page

--- user/examples/event.php
+++ user/examples/event.php
@@ -1,17 +1,4 @@
-<!--
- =========================================================
- Material Kit PRO - v2.2.0
- =========================================================
 
- Product Page: https://www.creative-tim.com/product/material-kit-pro
- Copyright 2019 Creative Tim (https://www.creative-tim.com)
-
- Coded by Creative Tim
-
- =========================================================
-
- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
--->
 <!DOCTYPE html>
 <html lang="en">
 
