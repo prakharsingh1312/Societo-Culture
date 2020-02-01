e9baa4b29aee217904b8a3da174df85494ebdd78
utkarsh chauhan <43794187+uchauhan2022@users.noreply.github.com>
2020-02-02 00:35:20 +0530

    add event

--- admin/examples/user.php
+++ admin/examples/user.php
@@ -1,136 +1,77 @@
-
-        <div class="container-fluid">
+<div class="container-fluid">
           <div class="row">
-            <div class="col-md-8">
-              <div class="card">
-                <div class="card-body">
-          <form>
-            <div class="row">
-              <div class="col-md-5 pr-1">
-                <div class="form-group">
-                  <label>User ID</label>
-                  <input type="text" class="form-control" disabled="" placeholder="User_ID" value="<?php echo $_SESSION['user_id'] ?>">
-                </div>
-              </div>
-              <div class="col-md-3 px-1">
-                <div class="form-group">
-                  <label>Username</label>
-                  <input type="text" class="form-control" disabled="" placeholder="Username" value="<?php echo $_SESSION['username'] ?>">
-                </div>
-              </div>
-              <div class="col-md-4 pl-1">
-                <div class="form-group">
-                  <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" class="form-control" placeholder="Email" value="<?php echo $_SESSION['user_email'] ?>">
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-12 pr-1">
-                <div class="form-group">
-                  <label>First Name</label>
-                  <input type="text" class="form-control" placeholder="First Name" value="<?php echo $_SESSION['user_name'] ?>">
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-12">
-                <div class="form-group">
-                  <label>Department</label>
-                  <input type="text" class="form-control" disabled="" placeholder="Home Address" value="<?php echo $_SESSION['user_dept'] ?>">
-                </div>
-              </div>
-            </div>
-            <!-- <div class="row">
-              <div class="col-md-4 pr-1">
-                <div class="form-group">
-                  <label>City</label>
-                  <input type="text" class="form-control" placeholder="City" value="Mike">
-                </div>
-              </div>
-              <div class="col-md-4 px-1">
-                <div class="form-group">
-                  <label>Country</label>
-                  <input type="text" class="form-control" placeholder="Country" value="Andrew">
-                </div>
-              </div>
-              <div class="col-md-4 pl-1">
-                <div class="form-group">
-                  <label>Postal Code</label>
-                  <input type="number" class="form-control" placeholder="ZIP Code">
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-12">
-                <div class="form-group">
-                  <label>About Me</label>
-                  <textarea rows="4" cols="80" class="form-control" placeholder="Here can be your description" value="Mike">Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo.</textarea>
-                </div>
-              </div>
-            </div> -->
-            <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#exampleModal">
-            Save Changes
-          </button>
-          </form>
-        </div>
-            </div>
-            <div class="col-md-4">
+            <div class="col-md-12">
               <div class="card">
                 <div class="card-header card-header-primary">
-                  <h4 class="card-title">Adding Announcements</h4>
-                  <p class="card-category">Enter the details of the announcement</p>
+                  <h4 class="card-title">Edit Profile</h4>
+                  <p class="card-category">Complete your profile</p>
                 </div>
                 <div class="card-body">
                   <form>
                     <div class="row">
-                      <div class="col-md-12">
+                      <div class="col-md-5">
                         <div class="form-group">
-                          <label class="bmd-label-floating">Society Name (FIXED)</label>
+                          <label class="bmd-label-floating">Society</label>
                           <input type="text" class="form-control" disabled>
                         </div>
                       </div>
-                    </div>
-                    <div class="row">
-                      <div class="col-md-6">
+                      <div class="col-md-3">
                         <div class="form-group">
-                          <label class="label-control">Start Date</label>
-                          <input type="text" class="form-control datetimepicker" value=""/>
+                          <label class="bmd-label-floating">Username</label>
+                          <input type="text" class="form-control">
                         </div>
                       </div>
-                      <div class="col-md-6">
+                      <div class="col-md-4">
                         <div class="form-group">
-                          <label class="label-control">End Date</label>
-                          <input type="text" class="form-control datetimepicker" value=""/>
+                          <label class="bmd-label-floating">Email address</label>
+                          <input type="email" class="form-control">
                         </div>
                       </div>
                     </div>
                     <div class="row">
                       <div class="col-md-12">
                         <div class="form-group">
-                          <label class="bmd-label-floating">Venue</label>
+                          <label class="bmd-label-floating">Full Name</label>
+                          <input type="text" class="form-control">
+                        </div>
+                      </div>
+                      
+                    </div>
+                    <div class="row">
+                      <div class="col-md-12">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Department</label>
                           <input type="text" class="form-control">
                         </div>
                       </div>
                     </div>
-                   
                     <div class="row">
-                      <div class="col-md-12">
+                      <div class="col-md-4">
                         <div class="form-group">
-                          <label>Announcement</label>
-                          <div class="form-group">
-                            <label class="bmd-label-floating"> Please add in depth details of the announcement</label>
-                            <textarea class="form-control" rows="5"></textarea>
-                          </div>
+                          <label class="bmd-label-floating">Password</label>
+                          <input type="Password" class="form-control">
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Mobile Number</label>
+                          <input type="text" class="form-control">
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Roll Number</label>
+                          <input type="text" class="form-control">
                         </div>
                       </div>
                     </div>
-                    <button type="submit" class="btn btn-primary pull-right">Announce!</button>
+                    
+                    <button type="submit" class="btn btn-primary pull-right">Update Profile</button>
                     <div class="clearfix"></div>
                   </form>
                 </div>
               </div>
             </div>
+           
             </div>
           </div>
-        
