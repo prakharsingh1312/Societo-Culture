a0b86840a4faaede5578254ecd54b870989c0843
utkarsh chauhan <43794187+uchauhan2022@users.noreply.github.com>
2020-02-01 17:38:58 +0530

    aa

--- user/examples/login-page.php
+++ user/examples/login-page.php
@@ -7,7 +7,7 @@ if(isset($_GET['login'])){
 }
 else{
 echo '<div class="page-header header-filter" style="background-image: url(\'assets/img/bg7.jpg\'); background-size: cover; background-position: top center;">
-<div id="notification_div"><div id="notification_inner_div"><div id="notification_inner_cell_div"></div></div></div>
+
     <div class="container">
       <div class="row">
         <div class="col-lg-4 col-md-6 col-sm-8 ml-auto mr-auto">
@@ -17,6 +17,7 @@ echo '<div class="page-header header-filter" style="background-image: url(\'asse
                 <h4 class="card-title">Login</h4>
                 
               </div>
+              <div id="notification_div"><div id="notification_inner_div"><div id="notification_inner_cell_div"></div></div></div>
               <div class="card-body ">
                 <p class="card-description text-center"></p>
                 
