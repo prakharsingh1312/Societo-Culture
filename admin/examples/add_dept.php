c21591f36b2a15af736e7927c073780c28a78790
utkarsh chauhan <43794187+uchauhan2022@users.noreply.github.com>
2020-02-02 00:48:27 +0530

    add event

---
+++ admin/examples/add_dept.php
@@ -0,0 +1,49 @@
+
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card">
+                <div class="card-header card-header-primary">
+                  <h4 class="card-title ">Department Table</h4>
+                  <p class="card-category"> List of all departments of the society</p>
+                </div>
+                <div class="card-body">
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead class=" text-primary">
+                        <th>
+                          Department ID
+                        </th>
+                        <th>
+                          Department Name
+                        </th>
+                        
+                        <th>
+                          Department Strength
+                        </th>
+                        
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>
+                            1
+                          </td>
+                          <td>
+                            XYZ
+                          </td>
+                          <
+                          <td class="text-primary">
+                            36,738
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+              </div>
+          
+        </div>
+      
