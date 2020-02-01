b5b2235fd079733f452856abda8f6f6fb6d28686
Prakhar Singh <prakharsingh13@gmail.com>
2020-02-01 14:01:35 +0530

    fix

---
+++ material-kit-pro-html-v2.2.0/header.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-color-on-scroll navbar-transparent    fixed-top  navbar-expand-lg " color-on-scroll="100" id="sectionsNav">
+    <div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" href="https://demos.creative-tim.com/material-kit-pro/index.html">
+          Material Kit PRO </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="material-icons">line_style</i> Upcoming Events
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="material-icons">notification_important</i> Announcements
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="material-icons">restore</i> Past Events
+            </a>
+          </li>
+          <li class="button-container nav-item iframe-extern">
+            <a href="https://www.creative-tim.com/product/material-kit-pro?ref=presentation" target="_blank" class="btn  btn-rose   btn-round btn-block">
+              <i class="material-icons">fingerprint</i> Login In
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
