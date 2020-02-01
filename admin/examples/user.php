73a24e152b2fd6c9c29fa79d37f47e901072f174
ccstiet <ccs@thapar.edu>
2020-02-02 03:09:21 +0530

    user profile page

--- admin/examples/user.php
+++ admin/examples/user.php
@@ -1,10 +1,16 @@
+<?php
+include '../../assets/php/dbconfig.php';
+?>
+
+
+
 <div class="container-fluid">
           <div class="row">
             <div class="col-md-12">
               <div class="card">
                 <div class="card-header card-header-primary">
-                  <h4 class="card-title">Edit Profile</h4>
-                  <p class="card-category">Complete your profile</p>
+                  <h4 class="card-title">Profile</h4>
+                  <!-- <p class="card-category">Complete your profile</p> -->
                 </div>
                 <div class="card-body">
                   <form>
@@ -12,56 +18,42 @@
                       <div class="col-md-5">
                         <div class="form-group">
                           <label class="bmd-label-floating">Society</label>
-                          <input type="text" class="form-control" disabled>
+                          <input type="text" class="form-control" value=" <?php echo $_SESSION['soc_name']; ?> " disabled>
                         </div>
                       </div>
                       <div class="col-md-3">
                         <div class="form-group">
                           <label class="bmd-label-floating">Username</label>
-                          <input type="text" class="form-control">
+                          <input type="text" class="form-control" value=" <?php echo $_SESSION['user_uname']; ?> " required>
                         </div>
                       </div>
                       <div class="col-md-4">
                         <div class="form-group">
                           <label class="bmd-label-floating">Email address</label>
-                          <input type="email" class="form-control">
+                          <input type="email" class="form-control" value=" <?php echo $_SESSION['user_email']; ?>  " required>
                         </div>
                       </div>
                     </div>
                     <div class="row">
                       <div class="col-md-12">
                         <div class="form-group">
-                          <label class="bmd-label-floating">Full Name</label>
-                          <input type="text" class="form-control">
+                          <label class="bmd-label-floating">Name</label>
+                          <input type="text" class="form-control" value=" <?php echo $_SESSION['user_name']; ?>  " >
                         </div>
                       </div>
                       
                     </div>
                     <div class="row">
-                      <div class="col-md-12">
-                        <div class="form-group">
-                          <label class="bmd-label-floating">Department</label>
-                          <input type="text" class="form-control">
-                        </div>
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="col-md-4">
+                      <!-- <div class="col-md-4">
                         <div class="form-group">
                           <label class="bmd-label-floating">Password</label>
                           <input type="Password" class="form-control">
                         </div>
-                      </div>
+                      </div> -->
                       <div class="col-md-4">
                         <div class="form-group">
                           <label class="bmd-label-floating">Mobile Number</label>
-                          <input type="text" class="form-control">
-                        </div>
-                      </div>
-                      <div class="col-md-4">
-                        <div class="form-group">
-                          <label class="bmd-label-floating">Roll Number</label>
-                          <input type="text" class="form-control">
+                          <input type="text" class="form-control" value=" <?php echo $_SESSION['user_mob']; ?>  ">
                         </div>
                       </div>
                     </div>
