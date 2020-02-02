1d9af0ea51585c70da504e93293dcaef9c9e60f5
ccstiet <ccs@thapar.edu>
2020-02-02 07:53:02 +0530

    event reminder admin

---
+++ admin/examples/query.php
@@ -0,0 +1,61 @@
+<?php
+include '../../assets/php/dbconfig.php';
+?>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card">
+                <div class="card-header card-header-primary">
+                  <h4 class="card-title ">List of all Task</h4>
+                  
+                </div>
+                <div class="card-body">
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead class=" text-primary">
+                        <th>
+                          Emails
+                        </th>
+                       
+                    
+                        
+                      </thead>
+                      <tbody>
+
+<?php
+
+$query="SELECT * FROM event_reminders";
+    $sq=mysqli_query($dbconfig,$query);
+    while($row=mysqli_fetch_array($sq)) {
+
+
+      echo "
+
+                        <tr>
+                          <td>
+                            ".$row['email']."
+                          </td>
+                     
+                         
+                        </tr>
+       
+
+      ";
+    }
+
+?>
+
+
+
+                      </tbody>
+                      <button class="btn btn-primary">SEND MAILS</button>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+</div>
+        
